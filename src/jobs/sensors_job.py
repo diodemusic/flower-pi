a3b2f4ec41866116c52f4503dabebb62a43a094d
diodemusic <diodemusicuk@gmail.com>
2025-09-11 00:07:59 +0100

    Initial commit

--- src/jobs/sensors_job.py
+++ src/jobs/sensors_job.py
@@ -4,9 +4,12 @@ import asyncio
 from src.controllers.sensors_controller import read_sensors
 from src.database.database import DB
 
-DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'database', 'flowerpi.db'))
+DB_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "database", "flowerpi.db")
+)
 db = DB(DB_PATH)
 db.init_db()
+
 
 async def sensor_job():
     while True:
