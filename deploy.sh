10f6d369f66bccd6f73f732c49125d63795bb2f5
diodemusic <diodemusicuk@gmail.com>
2025-09-12 00:44:44 +0100

    Use git clone instead of rsync

--- deploy.sh
+++ deploy.sh
@@ -8,24 +8,25 @@ fi
 
 echo "Deploying to $PI_USER@$PI_HOST at $REMOTE_DIR"
 
-rsync -avz --delete ./ "$PI_USER@$PI_HOST:$REMOTE_DIR"
-
 ssh "$PI_USER@$PI_HOST" << ENDSSH
-cd "$REMOTE_DIR"
 
-# Create venv if it doesn't exist
+rm -rf $REMOTE_DIR
+
+git clone $GIT_REPO
+
+cd $REMOTE_DIR
+
+# Create venv if missing
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
 
-source .venv/bin/activate
+source $VENV_DIR/bin/activate
 
-# Install dependencies
 pip install -r requirements.txt
 pip install fastapi[standard]
 
-# Make start.sh executable
-sudo chmod u+x start.sh
+chmod u+x start.sh
 
 echo "Deployment complete!"
 ENDSSH
