5b6bd55f0d46caebc59918ba6500ce135f0c37e4
diodemusic <diodemusicuk@gmail.com>
2025-09-11 02:58:49 +0100

    Fix FastAPI hang

--- src/main.py
+++ src/main.py
@@ -11,11 +11,11 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def start_sensor_job():
-    await sensor_job()
     asyncio.create_task(sensor_job())
 
 
 @app.get("/sensors")
 def read_sensors_route():
-    # Read some entries from the database
-    pass
+    res = read_sensors()
+
+    return res
