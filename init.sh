0613ff734b530e8e10dccee5d3bd27b725b4ee0f
diodemusic <diodemusicuk@gmail.com>
2025-09-11 02:04:40 +0100

    Seperate init logic from start script

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VENV_DIR=".venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+    echo "Virtual environment created in $VENV_DIR"
+else
+    echo "Virtual environment already exists in $VENV_DIR"
+fi
+
+source "$VENV_DIR/bin/activate"
+echo "Virtual environment activated"
+
+echo "Installing dependencies"
+pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-VENV_DIR=".venv"
-
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating virtual environment..."
-    python3 -m venv "$VENV_DIR"
-    echo "Virtual environment created in $VENV_DIR"
-else
-    echo "Virtual environment already exists in $VENV_DIR"
-fi
-
-source "$VENV_DIR/bin/activate"
-echo "Virtual environment activated"
-
-echo "Installing dependencies"
-pip install -r requirements.txt
-
-echo "Installing FastAPI"
-pip install fastapi[standard]
-
 echo "Starting API server"
 export PYTHONPATH=$(pwd)
 fastapi dev src/main.py
