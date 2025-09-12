d072e2484531fa9035d99f02380f9e99dba0834f
diodemusic <diodemusicuk@gmail.com>
2025-09-12 02:15:16 +0100

    Pass host

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 echo "Starting API server"
 export PYTHONPATH=$(pwd)
-fastapi dev src/main.py
+fastapi dev src/main.py --host $FASTAPI_HOST
