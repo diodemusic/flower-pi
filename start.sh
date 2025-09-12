b9f19643860f91fabdf77664fcaa570e17693773
diodemusic <diodemusicuk@gmail.com>
2025-09-12 02:20:51 +0100

    Remove environment variables

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
 echo "Starting API server"
 export PYTHONPATH=$(pwd)
-fastapi dev src/main.py --host $FASTAPI_HOST
+fastapi dev src/main.py
