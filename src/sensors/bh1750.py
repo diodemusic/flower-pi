75c642be669ac3367d543df02a735fa38fde1318
diodemusic <diodemusicuk@gmail.com>
2025-09-12 01:04:41 +0100

    Fix naming

--- src/sensors/bh1750.py
+++ src/sensors/bh1750.py
@@ -3,11 +3,11 @@ from smbus2 import SMBus
 
 class Light:
     def __init__(self):
-        self.BH1750_ADDR = 0x23
+        self.ADDR = 0x23
 
     def read_light(self):
         with SMBus(1) as bus:
-            b = bus.read_i2c_block_data(self.BH1750_ADDR, 0x10, 2)
+            b = bus.read_i2c_block_data(self.ADDR, 0x10, 2)
             raw = (b[0] << 8) | b[1]
             lux = raw / 1.2
             return lux
