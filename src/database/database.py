5be5f8fa747a037eaaa4bcebcdba41f2e444541b
diodemusic <diodemusicuk@gmail.com>
2025-09-13 05:39:56 +0100

    Maintain single concurrent connection

--- src/database/database.py
+++ src/database/database.py
@@ -5,11 +5,11 @@ import os
 class DB:
     def __init__(self, DB_PATH):
         self.DB_PATH = DB_PATH
+        self.con = sqlite3.connect(DB_PATH, check_same_thread=False)
+        self.cur = self.con.cursor()
 
     def init_db(self):
-        with sqlite3.connect(self.DB_PATH) as con:
-            cur = con.cursor()
-            cur.execute("""
+        self.cur.execute("""
                 CREATE TABLE IF NOT EXISTS readings (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -20,27 +20,23 @@ class DB:
                 )
             """)
 
-            con.commit()
+        self.con.commit()
 
     def save_reading(self, data):
-        with sqlite3.connect(self.DB_PATH) as con:
-            cur = con.cursor()
-            cur.execute(
-                "INSERT INTO readings (light, temp, pressure, humidity) VALUES (?, ?, ?, ?)",
-                (
-                    data.get("light", 0.00),
-                    data.get("temp", 0.00),
-                    data.get("pressure", 0.00),
-                    data.get("humidity", 0.00),
-                ),
-            )
+        self.cur.execute(
+            "INSERT INTO readings (light, temp, pressure, humidity) VALUES (?, ?, ?, ?)",
+            (
+                data.get("light", 0.00),
+                data.get("temp", 0.00),
+                data.get("pressure", 0.00),
+                data.get("humidity", 0.00),
+            ),
+        )
 
-            con.commit()
+        self.con.commit()
 
     def cleanup_old_readings(self):
-        with sqlite3.connect(self.DB_PATH) as con:
-            cur = con.cursor()
-            cur.execute("""
+        self.cur.execute("""
             DELETE FROM readings
             WHERE id NOT IN (
                 SELECT id FROM readings
@@ -49,16 +45,17 @@ class DB:
             )
             """)
 
-            con.commit()
+        self.con.commit()
 
     def get_readings(self):
-        with sqlite3.connect(self.DB_PATH) as con:
-            cur = con.cursor()
-            r = cur.execute(
-                'SELECT * FROM "readings" ORDER BY timestamp DESC LIMIT 100'
-            )
+        self.cur.execute("""
+                SELECT * FROM readings
+                ORDER BY timestamp
+                DESC LIMIT 100
+            """)
+        r = self.cur.fetchall()
 
-            return r
+        return r
 
 
 DB_PATH = os.path.abspath(
