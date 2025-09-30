15e8bae77d9761b2a2fad58de594e9a8bbff427d
diodemusic <diodemusicuk@gmail.com>
2025-09-30 19:47:34 +0100

    Deploy via rsync
    Deploying via GitHub was taking too long, this is faster and doesn't require pushing small changes every time i want to test anything

--- deploy.sh
+++ deploy.sh
@@ -6,26 +6,6 @@ if [ -f .env ]; then
 fi
 
 echo "Deploying to $PI_USER@$PI_HOST at $REMOTE_DIR"
-
-ssh "$PI_USER@$PI_HOST" << ENDSSH
-
-rm -rf $REMOTE_DIR
-
-git clone $GIT_REPO
-
-cd $REMOTE_DIR
-
-# Create venv if missing
-if [ ! -d ".venv" ]; then
-    python3 -m venv .venv
-fi
-
-source $VENV_DIR/bin/activate
-
-pip install -r requirements.txt
-pip install fastapi[standard]
-
-chmod u+x start.sh
+rsync -avz --delete ./ "$PI_USER@$PI_HOST:$REMOTE_DIR/"
 
 echo "Deployment complete!"
-ENDSSH
