0a9d4586913ab43c87d5f79c2ced30cd894f7e8b
diodemusic <diodemusicuk@gmail.com>
2025-09-30 20:07:29 +0100

    Fix dependencies not getting fetched on deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,23 @@ if [ -f .env ]; then
 fi
 
 echo "Deploying to $PI_USER@$PI_HOST at $REMOTE_DIR"
-rsync -avz --delete ./ "$PI_USER@$PI_HOST:$REMOTE_DIR/"
+rsync -avz --delete --exclude='.venv' --exclude='.vscode' ./ "$PI_USER@$PI_HOST:$REMOTE_DIR/"
+
+ssh "$PI_USER@$PI_HOST" << ENDSSH
+
+cd $REMOTE_DIR
+
+# Create venv if missing
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+
+source $VENV_DIR/bin/activate
+
+pip install -r requirements.txt
+pip install fastapi[standard]
+
+chmod u+x start.sh
 
 echo "Deployment complete!"
+ENDSSH
