c47fb7e78c633e761125a11b8c96c6dfd015402a
diodemusic <diodemusicuk@gmail.com>
2025-09-30 19:13:56 +0100

    Comment out BME280 sensor because i lost it lol

--- src/jobs/sensors_job.py
+++ src/jobs/sensors_job.py
@@ -1,24 +1,24 @@
 import asyncio
 
 from src.database.database import db
-from sensors import BH1750, BME280
+from sensors import BH1750  # , BME280
 
 bh1750 = BH1750.BH1750()
-bme280 = BME280.BME280()
+# bme280 = BME280.BME280()
 
 
 async def sensors_job():
     while True:
         light = bh1750.read_light()
-        temp = bme280.read_temp()
-        pressure = bme280.read_pressure()
-        humidity = bme280.read_humidity()
+        # temp = bme280.read_temp()
+        # pressure = bme280.read_pressure()
+        # humidity = bme280.read_humidity()
 
         data = {
             "light": light,
-            "temp": temp,
-            "pressure": pressure,
-            "humidity": humidity,
+            # "temp": temp,
+            # "pressure": pressure,
+            # "humidity": humidity,
         }
 
         db.save_reading(data)
